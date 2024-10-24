de0e44072f33997428da4d7baba6ed46cb6e4440
cksguddl0403 <cksgud0403@gmail.com>
2024-10-25 02:02:36 +0900

    Initial commit

---
+++ src/test/java/kr/triplan/TriplanApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.triplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriplanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
